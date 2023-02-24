45c92d0542dbb7a639af5c7b1f2657a0eff52430
Akshit Lalit <akshit.lalit@gmail.com>
2023-02-25 00:18:25 +0530

    fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-// const ejs=require("ejs");
+const ejs = require("ejs");
 const date = require(__dirname + "/date.js");
 
 const app = express();
